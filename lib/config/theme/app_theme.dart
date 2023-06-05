8ba9c763feb8648bce2b65bfa2c2523839c3081c
yvillegas <villegasyanina.c@gmail.com>
2023-06-05 17:10:14 -0500

    theme settings

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final List<Color> colorList = [
+  Colors.black,
+  Color.fromARGB(255, 224, 128, 219),
+  Colors.white,
+  Colors.blue,
+  Colors.purple,
+  Color.fromARGB(255, 217, 20, 207)
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor > 0, 'Selected color must be greater the 0'),
+        assert(selectedColor < colorList.length, 'Selected color must be less');
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+}
